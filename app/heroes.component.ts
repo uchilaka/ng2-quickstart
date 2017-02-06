d49fea21e54bead2cda8ba64daeb42b578b20f72
Uchenna Chilaka <uche.chilaka@gmail.com>
2017-02-06 00:06:50 -0500

    Nothing to see here. Just updating...

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -46,7 +46,7 @@ export class HeroesComponent implements OnInit {
     this.heroService
       .delete(hero.id)
       .then(() => {
-        this.heroes = this.heroes.filer(h => h !== hero);
+        this.heroes = this.heroes.filter(h => h !== hero);
         if (this.selectedHero === hero) { this.selectedHero = null; }
       });
   }
